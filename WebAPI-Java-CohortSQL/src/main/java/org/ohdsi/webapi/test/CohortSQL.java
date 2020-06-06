1d925ebedc76a37fb9d345dadf1346e9d564d456
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-06-06 12:25:47 +0300

    initial commit

---
+++ WebAPI-Java-CohortSQL/src/main/java/org/ohdsi/webapi/test/CohortSQL.java
@@ -0,0 +1,13 @@
+package org.ohdsi.webapi.test;
+
+public class CohortSQL {
+    private String templateSql;
+
+    public String getTemplateSql() {
+        return templateSql;
+    }
+
+    public void setTemplateSql(String templateSql) {
+        this.templateSql = templateSql;
+    }
+}
